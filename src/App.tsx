fe6b255c43ea89be0ca0f4891a9dfa0caa94c2d7
MtMadness08 <soupeater56@gmail.com>
2026-02-24 17:03:57 -0500

    added button

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const handleClick = () => {
+    alert("Button was clicked!");
+  };
+
   return (
     <div className="App">
         <h1>Testing!</h1>
         <h3>Alex Wuz Here...</h3>
+
+        <button onClick={handleClick}>
+          Click Me
+        </button>
+
     </div>
   );
 }
