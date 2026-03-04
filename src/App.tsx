972a05abf27d88a8ce0972a93ed0d539cbf0434e
zacharynarcotta <zachary.narcotta@maine.edu>
2026-03-04 15:17:17 -0500

    Add navigation bar, CSS formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-  const handleClick = () => {
-    alert("Button was clicked!");
-  };
-
   return (
-    <div className="App">
-        <h1>Testing!</h1>
-        <h3>Alex Wuz Here...so wuz Turner</h3>
-
-        <button onClick={handleClick}>
-          Click Me
-        </button>
-
+    <div className="app-page">
+      <div className="app-navbar">
+        <nav>
+          <ul className="app-navbar-list">
+            <li className="app-navbar-item">
+              <a href="#home">Home</a>
+            </li>
+            <li className="app-navbar-item">
+              <a href="#search">Search</a>
+            </li>
+            <li className="app-navbar-item">
+              <a href="#cards">My Cards</a>
+            </li>
+            <li className="app-navbar-item">
+              <a href="#collections">Collections</a>
+            </li>
+          </ul>
+          <ul>
+            <div>
+              <ul className="app-navbar-account">
+                <li className="app-navbar-item">
+                  <a href="#sign-up">Sign Up</a>
+                </li>
+                <li className="app-navbar-item">
+                  <a href="#log-in">Log In</a>
+                </li>
+              </ul>
+            </div>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
