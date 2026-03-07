68daf8537462279cbb6d98c2698b9ed032c9718f
zacharynarcotta <zachary.narcotta@maine.edu>
2026-03-07 14:54:45 -0500

    Update import

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import Account from "./pages/account";
+import Account from "./pages/account/account";
 import Navbar from "./components/navbar/navbar";
 
 function App(): React.JSX.Element {
   return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <Navbar />
+    <div className="app">
+      <BrowserRouter>
+        {/* Navigation */}
+        <Navbar />
 
-      {/* Site Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </BrowserRouter>
+        {/* Site Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
