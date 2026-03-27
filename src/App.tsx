fc32c3b3325577bc54ce619cc0c5027b0ee8f3d4
zacharynarcotta <zachary.narcotta@maine.edu>
2026-03-27 14:52:20 -0400

    Fixed merge discrepency pushed to repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,5 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< account-page-branch
-import Home from "./pages/home";
-import Account from "./pages/account/account";
-import Signup from "./pages/account/signup";
-import LoginPage from "./pages/account/login";
-import Navbar from "./components/Navbar/Navbar";
-
-function App(): React.JSX.Element {
-  const [token, setToken] = useState();
-
-  return (
-    <div className="app">
-      <BrowserRouter>
-        {/* Navigation */}
-        <Navbar />
-
-        {/* Site Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/account/sign-up" element={<Signup />} />
-          <Route
-            path="/account/log-in"
-            element={<LoginPage setToken={setToken} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-=======
 
 import Home from "./pages/home";
 import Collection from "./pages/CardCollection";
@@ -36,7 +7,6 @@ import Search from "./pages/CardSearch";
 import Navbar from "./components/navbar/navbar";
 
 function App() {
-
   // Stores all cards in the collection
   const [cards, setCards] = useState<any[]>([]);
 
@@ -62,15 +32,11 @@ function App() {
           element={<Collection cards={cards} removeCard={removeCard} />}
         />
 
-        <Route
-          path="/search"
-          element={<Search addCard={addCard} />}
-        />
+        <Route path="/search" element={<Search addCard={addCard} />} />
 
         <Route path="/all" element={<Home />} />
       </Routes>
     </BrowserRouter>
->>>>>>> sprint-2
   );
 }
 
