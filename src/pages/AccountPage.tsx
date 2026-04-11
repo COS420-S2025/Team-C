a1fc521a8eddd3f7924604517e726a651fc075fb
zacharynarcotta <zachary.narcotta@maine.edu>
2026-04-11 18:28:31 -0400

    Implement changing email

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -13,12 +13,18 @@ export type AccountPageDisplayProps = {
 const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
   const [activateSignup, setActivateSignup] = useState<boolean>(true);
   const [newName, setNewName] = useState<string>("");
+  const [newEmail, setNewEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
 
   // I don't know what type is sent when changing
   // an input field, if someone knows or can
   // find out, please change this type declaration
   const handleNameChange = (event: any) => {
     setNewName(event.target.value);
+  };
+
+  const handleEmailChange = (event: any) => {
+    setNewEmail(event.target.value);
   };
 
   const handleNameUpdate = async () => {
@@ -30,7 +36,27 @@ const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
           name: newName,
         });
         setUserData({ ...userData, name: newName });
+        setMessage(`Name changed to: ${newName}!`);
         setNewName("");
+      } catch (e) {
+        if (!(e instanceof Error)) {
+          throw e;
+        }
+
+        console.error(e.message);
+      }
+    }
+  };
+
+  const handleEmailUpdate = async () => {
+    if (userData) {
+      try {
+        const userRef = doc(db, "users", userData.uid);
+
+        await updateDoc(userRef, { email: newEmail });
+        setUserData({ ...userData, email: newEmail });
+        setMessage(`Email changed to: ${newEmail}!`);
+        setNewEmail("");
       } catch (e) {
         if (!(e instanceof Error)) {
           throw e;
@@ -56,6 +82,7 @@ const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
               <h3 className="text-3xl font-bold mb-2">
                 Signed in as: {userData.name}
               </h3>
+              {message && <div className="text-lime-400 mb-4">{message}</div>}
               <h2 className="text-lg mb-4">Configuration:</h2>
               <div>
                 <input
@@ -70,6 +97,21 @@ const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
                   onClick={handleNameUpdate}
                 >
                   Change Name
+                </button>
+              </div>
+              <div>
+                <input
+                  type="email"
+                  value={newEmail}
+                  className="w-full text-white py-2 mb-4 bg-transparent border-b border-gray-500 focus:outline-none focus:border-white"
+                  placeholder="New Email"
+                  onChange={handleEmailChange}
+                />
+                <button
+                  className="w-full bg-transparent border border-white text-white my-2 font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer"
+                  onClick={handleEmailUpdate}
+                >
+                  Change Email
                 </button>
               </div>
             </div>
