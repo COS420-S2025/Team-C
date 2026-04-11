51ea81dfde6dc13fe3bf77477e8ad462f1f31127
zacharynarcotta <zachary.narcotta@maine.edu>
2026-04-11 18:06:39 -0400

    Add basic field to update display name

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Login from "../components/Login/Login";
 import Signup from "../components/Signup/Signup";
 import type { AccountProps } from "../components/Navbar/Navbar";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "..";
 
 export type AccountPageDisplayProps = {
   AccountProps: AccountProps;
@@ -10,6 +12,33 @@ export type AccountPageDisplayProps = {
 
 const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
   const [activateSignup, setActivateSignup] = useState<boolean>(true);
+  const [newName, setNewName] = useState<string>("");
+
+  // I don't know what type is sent when changing
+  // an input field, if someone knows or can
+  // find out, please change this type declaration
+  const handleNameChange = (event: any) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNameUpdate = async () => {
+    if (userData) {
+      try {
+        const userRef = doc(db, "users", userData.uid);
+
+        await updateDoc(userRef, {
+          name: newName,
+        });
+        setUserData({ ...userData, name: newName });
+      } catch (e) {
+        if (!(e instanceof Error)) {
+          throw e;
+        }
+
+        console.error(e.message);
+      }
+    }
+  };
 
   // signed in
   if (userData) {
@@ -23,8 +52,21 @@ const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
           <div className="w-full flex flex-col max-w-[450px] mx-auto">
             {/* Header */}
             <div className="w-full flex flex-col mb-10 text-white">
-              <h3 className="text-4xl font-bold mb-2">Nice!</h3>
-              <p className="text-lg mb-4">You are signed in!</p>
+              <h3 className="text-3xl font-bold mb-2">
+                Signed in as: {userData.name}
+              </h3>
+              <h2 className="text-lg mb-4">Configuration:</h2>
+              <div>
+                <p>Update display name:</p>
+
+                <input
+                  type="text"
+                  className="text-black"
+                  placeholder="New Name"
+                  onChange={handleNameChange}
+                />
+                <button onClick={handleNameUpdate}>Change</button>
+              </div>
             </div>
           </div>
         </div>
