5e487bcd83580c397a236f7532b57aea70774631
zacharynarcotta <zachary.narcotta@maine.edu>
2026-04-11 15:48:18 -0400

    Add base account landing page

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -9,7 +9,28 @@ export type AccountPageDisplayProps = {
 };
 
 const Account: React.FC<AccountProps> = ({ userData, setUserData }) => {
-  const [activateSignup, setActivateSignup] = useState<boolean>(false);
+  const [activateSignup, setActivateSignup] = useState<boolean>(true);
+
+  // signed in
+  if (userData) {
+    return (
+      <div className="w-full h-screen flex">
+        {/* Left side */}
+        <div className="w-1/2 h-full flex flex-col bg-[#282c34] items-center justify-center"></div>
+
+        {/* Right side */}
+        <div className="w-1/2 h-full flex flex-col bg-[#1a1a1a] p-20 justify-center">
+          <div className="w-full flex flex-col max-w-[450px] mx-auto">
+            {/* Header */}
+            <div className="w-full flex flex-col mb-10 text-white">
+              <h3 className="text-4xl font-bold mb-2">Nice!</h3>
+              <p className="text-lg mb-4">You are signed in!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return !activateSignup ? (
     <Login
