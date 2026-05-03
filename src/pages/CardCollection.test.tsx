2c0afa8bced8aadd31c51bdf03741b3db35732fe
Tsimis <157445349+Tsimis@users.noreply.github.com>
2026-05-03 10:14:26 -0400

    Added test to check is popup is closed on collection page

--- src/pages/CardCollection.test.tsx
+++ src/pages/CardCollection.test.tsx
@@ -3,42 +3,39 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import CollectionPage from "./CardCollection";
 import type { User } from "../interfaces/User";
 
-//Test to check if Pop up appears when Create New Collection button is clicked
 jest.mock("../components/CollectionWindow/CollectionWindow", () => ({
   CollectionWindow: ({ onClose }: { onClose: () => void }) => (
     <div>
       <p>Mock Create Collection Popup</p>
-      <button onClick={onClose}>Close Popup</button>
+      <button onClick={onClose}>x</button>
     </div>
   ),
 }));
 
-jest.mock("..", () => ({ auth: {}, db: {} }));
-jest.mock("firebase/auth", () => ({
-  onAuthStateChanged: () => () => {},
-}));
-jest.mock("firebase/firestore", () => ({
-  collection: jest.fn(),
-  getDocs: jest.fn().mockResolvedValue({ empty: true, forEach: jest.fn() }),
-  doc: jest.fn(),
-  getDoc: jest.fn(),
-}));
+const fakeUser: User = {
+  uid: "1234",
+  name: "Testy McUser",
+  email: "test@example.com",
+};
 
-describe("CardCollection create collection popup", () => {
-  test('clicking "Create New Collection" shows popup', () => {
-    const fakeUser: User = {
-      uid: "1234",
-      name: "Testy McUser",
-      email: "test@example.com",
-    };
-    render(<CollectionPage userData={fakeUser} />);
+//Test to check if Pop up appears when Create New Collection button is clicked
+test('clicking "Create New Collection" shows popup', () => {
+  render(<CollectionPage userData={fakeUser} />);
 
-    expect(screen.queryByText("Mock Create Collection Popup")).toBeNull();
+  expect(screen.queryByText("Mock Create Collection Popup")).toBeNull();
 
-    fireEvent.click(screen.getByText("Create New Collection"));
+  fireEvent.click(screen.getByText("Create New Collection"));
 
-    expect(
-      screen.getByText("Mock Create Collection Popup"),
-    ).toBeInTheDocument();
-  });
+  expect(screen.getByText("Mock Create Collection Popup")).toBeInTheDocument();
+});
+
+//Test to check if Popup can be closed by clicking a mock "x" button, open and closes Popup
+test('clicking "x" hides closes Popup', () => {
+  render(<CollectionPage userData={fakeUser} />);
+
+  fireEvent.click(screen.getByText("Create New Collection"));
+  expect(screen.getByText("Mock Create Collection Popup")).toBeInTheDocument();
+
+  fireEvent.click(screen.getByText("x"));
+  expect(screen.queryByText("Mock Create Collection Popup")).toBeNull();
 });
